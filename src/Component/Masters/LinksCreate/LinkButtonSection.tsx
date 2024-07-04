3cecd80ec669775f1ff7f0a51af471b17dde979c
ramanathan-m <ramanathan120599@gmail.com>
2024-07-04 10:24:47 +0530

    ButtonChanges

--- src/Component/Masters/LinksCreate/LinkButtonSection.tsx
+++ src/Component/Masters/LinksCreate/LinkButtonSection.tsx
@@ -9,7 +9,9 @@ const LinkButtonSection = () => {
         <Row className="justify-content-center">
           <Col md="6" className="d-flex flex-column col-md-6 ms-5 ">
             <div className="text-end offset-sm-1">
+            <Link to={`${process.env.PUBLIC_URL}/masters/links`}>
               <Btn color="primary" className="me-3">{Add}</Btn>
+              </Link>
               <Link to={`${process.env.PUBLIC_URL}/masters/links`}>
                 <Btn color="light">{Cancel}</Btn>
               </Link>
--- src/Component/Masters/VendorCreate/VendorButtonSection.tsx
+++ src/Component/Masters/VendorCreate/VendorButtonSection.tsx
@@ -9,7 +9,9 @@ const VendorButtonSection = () => {
         <Row className="justify-content-center">
           <Col md="6" className="d-flex flex-column col-md-6 ms-5 ">
             <div className="text-end offset-sm-1">
+            <Link to={`${process.env.PUBLIC_URL}/masters/vendors`}>
               <Btn color="primary" className="me-3">{Add}</Btn>
+              </Link>
               <Link to={`${process.env.PUBLIC_URL}/masters/vendors`}>
                 <Btn color="light">{Cancel}</Btn>
               </Link>
